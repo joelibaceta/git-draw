c542e6ddd7ead637a9f738c5c1d80e82bb3e0569
Joel Ibaceta <mail@joelibaceta.com>
2020-05-07 12:01:56 -0500

    Delete deploy-extension.sh

--- deploy-extension.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rm git-draw.zip
-find . -name "*.un~" -exec rm -rf {} \;
-zip -r git-draw.zip extension/* -x "*.DS_Store"
-
-#TODO: Figure out how to deploy to chrome developer dashboard via api
